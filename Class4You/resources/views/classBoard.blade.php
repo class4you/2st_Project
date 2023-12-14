3336a9a48a1f7b050c87101ef0792f8d38f5e16c
choibocknami12 <h_hcho1215@naver.com>
2023-12-14 12:48:23 +0900

    1214강의게시판laravel

--- Class4You/resources/views/classBoard.blade.php
+++ Class4You/resources/views/classBoard.blade.php
@@ -1,6 +1,21 @@
-
-
-
-@section('classBoard')
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="">
+	<title>classBoard</title>
+</head>
+<body>
 	
-@endsection
+	<div>
+		<Header-Component></Header-Component>
+
+		@yield('classBoardMain')
+
+		<Footer-Component></Footer-Component>
+	</div>
+
+</body>
+</html>
