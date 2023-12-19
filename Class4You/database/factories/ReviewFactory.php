6a6ba0b4f3c55ea80cc32b5fc69293a3783f7af9
iammyeongho <jmh4912@naver.com>
2023-12-19 14:55:43 +0900

    1219 레이아웃 및 더미 데이터 작업

--- Class4You/database/factories/ReviewFactory.php
+++ Class4You/database/factories/ReviewFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Review;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
@@ -16,8 +17,13 @@ class ReviewFactory extends Factory
      */
     public function definition()
     {
+        $date = $this->faker->dateTimeBetween('-15 days', '+19 days');
         return [
-            //
+            'EnrollmentID' => $this->faker->numberBetween(1, 97),
+            'ReviewComment' => $this->faker->realText(200),
+            'ReviewRating' => $this->faker->randomElement([1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]),
+            'created_at' => $date,
+            'updated_at' => $date,
         ];
     }
 }
