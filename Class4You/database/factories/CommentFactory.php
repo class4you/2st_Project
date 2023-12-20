74fc796459832cc4c05ec6188bc5ef653e336863
iammyeongho <jmh4912@naver.com>
2023-12-20 17:31:52 +0900

    1220 보더 확인

--- Class4You/database/factories/CommentFactory.php
+++ Class4You/database/factories/CommentFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Comment;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
@@ -16,8 +17,13 @@ class CommentFactory extends Factory
      */
     public function definition()
     {
+        $date = $this->faker->dateTimeBetween('-1 years');
         return [
-            //
+            'UserID' => $this->faker->randomElement([8, 9, 10, 11]),
+            'BoardID' =>  $this->faker->numberBetween(301, 606),
+            'CommentContent' => $this->faker->realText(200),
+            'created_at' => $date,
+            'updated_at' => $date,
         ];
     }
 }
