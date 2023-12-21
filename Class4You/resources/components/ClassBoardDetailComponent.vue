1214c328084eab57337098cc8a19fec22a120cd2
choibocknami12 <h_hcho1215@naver.com>
2023-12-21 18:11:31 +0900

    1221css

--- Class4You/resources/components/ClassBoardDetailComponent.vue
+++ Class4You/resources/components/ClassBoardDetailComponent.vue
@@ -71,8 +71,8 @@
 				<ul class="class_tabs">
 					<li @click="clickFlgTab = 0;" class="class_tab_link current" data-tab="class_tab1">강의 소개</li>
 					<li @click="clickFlgTab = 1;" class="class_tab_link" data-tab="class_tab2">수강평</li>
-					<li @click="clickFlgTab = 2;" class="class_tab_link" data-tab="class_tab3">커뮤니티</li>
-					<li @click="clickFlgTab = 3;" class="class_tab_link" data-tab="class_tab4">공지사항</li>
+					<!-- <li @click="clickFlgTab = 2;" class="class_tab_link" data-tab="class_tab3">커뮤니티</li> -->
+					<!-- <li @click="clickFlgTab = 3;" class="class_tab_link" data-tab="class_tab4">공지사항</li> -->
 				</ul>
 			</div>	
 
