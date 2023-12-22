21e0ffaa0ab15f050909d8284e8cdfffe01735bd
iammyeongho <jmh4912@naver.com>
2023-12-22 20:51:29 +0900

    1222 마이페이지 작업

--- Class4You/app/Http/Controllers/MypageController.php
+++ Class4You/app/Http/Controllers/MypageController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Log;
 use App\Models\User;
 use App\Models\Enrollment;
 use App\Models\Classinfo;
+use App\Models\Chapter;
+use App\Models\Lesson;
 // use App\Models\Board;
 
 class MypageController extends Controller
@@ -29,7 +31,55 @@ class MypageController extends Controller
             ->where('boards.UserID', $UserID)
             ->get();
 
-        Log::debug($boardData);
+        $enrollments = Enrollment::where('UserID', $UserID)->get();
+
+        $classIDs = $enrollments->pluck('ClassID');
+
+        $chapters = collect();
+
+        foreach ($classIDs as $classID) {
+            $classChapters = Chapter::where('ClassID', $classID)->get();
+            if (!$classChapters->isEmpty()) {
+                foreach ($classChapters as $chapter) {
+                    $chapterID = $chapter->ChapterID;
+        
+                    // 챕터 아이디를 기반으로 해당 챕터에 속하는 리슨을 가져옴
+                    $Lessons = Lesson::where('ChapterID', $chapterID)->get();
+
+                    // 현재 챕터에 대한 리슨 값을 별도의 배열로 저장
+                    $chapter->Lessons = $Lessons->toArray();
+
+                    // $chapter 객체에는 현재 챕터와 그에 대한 리슨 값들이 별도의 배열로 포함됨
+                    Log::debug($chapter);
+                }
+            }
+        }
+        Log::debug($chapter);
+        // Log::debug($chapters);
+        
+        
+        // $chapters = Chapter::where('ClassID', $classIDs)
+        //     ->get();
+
+        // Log::debug($enrollments);
+        // Log::debug($classIDs);
+        // $chapters = Chapter::where('class_id', $classId)->get();
+        // $lessons = Lesson::whereIn('chapter_id', $chapters->pluck('id'))->get();
+
+        // $totalChapters = $chapters->count();
+        // $completedChapters = $chapters->filter(function ($chapter) {
+        //     return $chapter->chapter_flg == 1;
+        // })->count();
+
+        // $totalLessons = $lessons->count();
+        // $completedLessons = $lessons->filter(function ($lesson) {
+        //     return $lesson->LessonFlg == 1;
+        // })->count();
+
+        // $classProgress = ($completedLessons / $totalLessons) * 100;
+    
+
+        // Log::debug($boardData);
 
         return response()->json([
             'userData' => $userData,
