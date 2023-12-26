7b0ffd43d8452a6cf584b817a68916b3b0cda3b4
choibocknami12 <h_hcho1215@naver.com>
2023-12-26 21:32:56 +0900

    1226css

--- Class4You/resources/components/ClassBoardDetailComponent.vue
+++ Class4You/resources/components/ClassBoardDetailComponent.vue
@@ -71,9 +71,9 @@
         <div class="class_container">
             <!-- 강의 소개 -->
 			<div class="class_tab_menu_div">
-				<ul class="class_tabs">
-					<li @click="clickFlgTab = 0;" @mouseover="clickTab" class="class_tab_link current" data-tab="class_tab1">강의 소개</li>
-					<li @click="clickFlgTab = 1;" class="class_tab_link" data-tab="class_tab2">수강평</li>
+				<ul @mouseover="clickTab" class="class_tabs">
+					<li @click="clickFlgTab = 0;" :class="{ 'hovered': hovered }" class="class_tab_link current" data-tab="class_tab1">강의 소개</li>
+					<li @click="clickFlgTab = 1;" :class="{ 'hovered': hovered }" class="class_tab_link" data-tab="class_tab2">수강평</li>
 					<!-- <li @click="clickFlgTab = 2;" class="class_tab_link" data-tab="class_tab3">커뮤니티</li> -->
 					<!-- <li @click="clickFlgTab = 3;" class="class_tab_link" data-tab="class_tab4">공지사항</li> -->
 				</ul>
@@ -522,6 +522,7 @@ export default {
 			enrollmentCnt: {},
 			// pagination: {},
 			// page: {},
+			hovered: false,
         }
     },
 	mounted() {
@@ -684,12 +685,15 @@ export default {
 		},
 		
 		clickTab() {
-
+			this.hovered = true;
 		},
 	},
     
 }
 </script>
 <style>
-    
+    .class_tab_link.hovered {
+		/* color: #222; */
+    	border-bottom: 2px solid #000;
+	}
 </style>
