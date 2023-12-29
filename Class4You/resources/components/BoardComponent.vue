9a9a9e7ffeff9ac8447e74af84d2da49b75c1898
iammyeongho <jmh4912@naver.com>
2023-12-29 12:13:22 +0900

    1229 페이징 처리

--- Class4You/resources/components/BoardComponent.vue
+++ Class4You/resources/components/BoardComponent.vue
@@ -92,22 +92,16 @@
 									</select>
 									<ul class="order_pc">
 									<li class="e_order active">
-										<button type="button" @click="fetchData(page = 1, searchQuery, solve = null, sortData = 1)" class="order_pc_button" :class="{ 'order_pc_button_on': sortData == 1}"><svg  style="opacity: 0.3;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="violet" class="bi bi-dot" viewBox="0 0 16 16">
-  <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
-</svg>최신순</button>
+										<button type="button" @click="fetchData(page = 1, searchQuery, solve = null, sortData = 1)" class="order_pc_button" :class="{ 'order_pc_button_on': sortData == 1}"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="violet" class="bi bi-dot svg_opacity_off" :class="{ 'svg_opacity_on': sortData == 1}" viewBox="0 0 16 16"><path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/></svg>최신순</button>
 									</li>
 									<!-- <li class="e_order active">
 										<button type="button" @click="fetchData(page = 1, searchQuery, solve = null, sortData = 2)" class="order_pc_button">답변많은순</button>
 									</li> -->
 									<li class="e_order active">
-										<button type="button" @click="fetchData(page = 1, searchQuery, solve = null, sortData = 3)" class="order_pc_button" :class="{ 'order_pc_button_on': sortData == 3}"><svg style="opacity: 0.3;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="violet" class="bi bi-dot" viewBox="0 0 16 16">
-  <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
-</svg>좋아요순</button>
+										<button type="button" @click="fetchData(page = 1, searchQuery, solve = null, sortData = 3)" class="order_pc_button" :class="{ 'order_pc_button_on': sortData == 3}"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="violet" class="bi bi-dot svg_opacity_off" :class="{ 'svg_opacity_on': sortData == 3}" viewBox="0 0 16 16"><path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/></svg>좋아요순</button>
 									</li>
 									<li class="e_order active">
-										<button type="button" @click="fetchData(page = 1, searchQuery, solve = null, sortData = 4)" class="order_pc_button" :class="{ 'order_pc_button_on': sortData == 4}"><svg  style="opacity: 0.3;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="violet" class="bi bi-dot" viewBox="0 0 16 16">
-  <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
-</svg>조회순</button>
+										<button type="button" @click="fetchData(page = 1, searchQuery, solve = null, sortData = 4)" class="order_pc_button" :class="{ 'order_pc_button_on': sortData == 4}"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="violet" class="bi bi-dot svg_opacity_off" :class="{ 'svg_opacity_on': sortData == 4}" viewBox="0 0 16 16"><path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/></svg>조회순</button>
 									</li>
 									</ul>
 									<div class="posts_container_header_button_cover"></div>
@@ -179,7 +173,7 @@
 								<div class="qustuon_list_pageing">
 									<div v-for="(page, index) in pagination" :key="index">
 										<template v-if="page.url !== null">
-											<a @click.prevent="fetchData(page.label)" href="#">{{ replaceString(page.label) }}</a>
+											<a class="qustuon_list_page_a" :class="{'page_on': page.label == pageChk}" @click.prevent="fetchData(page.label)" href="#">{{ replaceString(page.label) }}</a>
 										</template>
 										<template v-else>
 											<span>{{ replaceString(page.label) }}</span>
@@ -191,20 +185,20 @@
 							<div class="community_right_side">
 								<div class="ranking_container">
 								<p class="weeklyranking">주간 답변 랭킹  <svg style="opacity: 0.5;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="blue" class="bi bi-trophy" viewBox="0 0 16 16">
-  <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935zM3.504 1c.007.517.026 1.006.056 1.469.13 2.028.457 3.546.87 4.667C5.294 9.48 6.484 10 7 10a.5.5 0 0 1 .5.5v2.61a1 1 0 0 1-.757.97l-1.426.356a.5.5 0 0 0-.179.085L4.5 15h7l-.638-.479a.501.501 0 0 0-.18-.085l-1.425-.356a1 1 0 0 1-.757-.97V10.5A.5.5 0 0 1 9 10c.516 0 1.706-.52 2.57-2.864.413-1.12.74-2.64.87-4.667.03-.463.049-.952.056-1.469H3.504z"/>
-</svg></p>
+									<path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935zM3.504 1c.007.517.026 1.006.056 1.469.13 2.028.457 3.546.87 4.667C5.294 9.48 6.484 10 7 10a.5.5 0 0 1 .5.5v2.61a1 1 0 0 1-.757.97l-1.426.356a.5.5 0 0 0-.179.085L4.5 15h7l-.638-.479a.501.501 0 0 0-.18-.085l-1.425-.356a1 1 0 0 1-.757-.97V10.5A.5.5 0 0 1 9 10c.516 0 1.706-.52 2.57-2.864.413-1.12.74-2.64.87-4.667.03-.463.049-.952.056-1.469H3.504z"/>
+								</svg></p>
 								<ul class="ranking_weekly_popular_writes_list">
 									<li class="ranking_weekly_popular_write">
-									<div>
-										<ol v-for="(item, index) in newUserCntItems" :key="item.id" class="ranking_weekly_popular_write_list">
-											<li class="ranking_weekly_popular_write_writer">{{ index + 1 }}. {{ hideEmail(item.UserEmail) }}</li>
-											<li class="ranking_weekly_popular_write_writer">답변 수 : {{ item.cnt }}</li>
-										</ol>
-									</div>
+										<div>
+											<ol v-for="(item, index) in newUserCntItems" :key="item.id" class="ranking_weekly_popular_write_list">
+												<li class="ranking_weekly_popular_write_writer">{{ index + 1 }}. {{ hideEmail(item.UserEmail) }}</li>
+												<li class="ranking_weekly_popular_write_writer">답변 수 : {{ item.cnt }}</li>
+											</ol>
+										</div>
 									</li>
 								</ul>
-								</div>
 							</div>
+						</div>
 							</section>
 						</div>
 				</div>
@@ -230,6 +224,8 @@ export default {
 			searchQuery: '',
 			solve: null,
 			sortData: 1,
+			current_page: 1,
+			pageChk: {},
 		}
 	},
 	
@@ -245,6 +241,7 @@ export default {
                 this.newUserCntItems = response.data.userCntData;
                 this.pagination = response.data.boardData.links;
                 this.page = response.data.boardData.current_page;
+                this.pageChk = response.data.boardData.current_page;
 				// console.log(response.data.boardData.links);
 				// console.log(response.data.boardData.current_page);
 				console.log(response.data);
