ccf2468172377ec255a61509e5e90f4bc4787f1d
iammyeongho <jmh4912@naver.com>
2024-01-02 12:04:25 +0900

    1002 레이아웃 작업

--- Class4You/resources/components/ClassBoardDetailComponent.vue
+++ Class4You/resources/components/ClassBoardDetailComponent.vue
@@ -163,7 +163,8 @@
 					<div class="class_tab_content_title">
 						<p>커리큘럼</p>
 					</div>
-					<div v-for="item in allLessonsData" id="class_tab1" class="class_tab_curri_div">
+					<!-- <div v-for="item in allLessonsData" id="class_tab1" class="class_tab_curri_div">
+						<details>
 						<div class="class_tab_content_curriculum">
 							<div class="class_tab_content_curriculm_title" style="margin: 10px 0px;">
 								<span class="class_tab_content_chapter_title">Charpter.</span>
@@ -171,20 +172,34 @@
 									{{ item.ChapterTitle }}
 								</span>
 							</div>
-							<div v-for="item2 in item.lessons" class="class_tab_content_lesson_title">
-								<span>Lesson</span>
-								<p>
-									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
-  										<path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z"/>
-									</svg>
-									{{ item2.LessonTitle }}
-								</p>
+							<div v-for="(item2, item2Index) in item.lessons" class="class_tab_content_lesson_title">
+								<p>{{item2Index + 1}}.lessons : {{ item2.LessonTitle }}</p>
 							</div>
 							<div class="class_tab_content_lesson_content">
 								<p>{{ classCuriData.LessonContent }}</p>
 							</div>
 						</div>
+					</details>
+					</div> -->
+
+					<div v-for="item in allLessonsData" id="class_tab1" class="class_tab_curri_div">
+						<details class="class_tab_content_curriculum">
+							<summary class="class_tab_content_curriculm_title" style="margin: 10px 0px;">
+								<span class="class_tab_content_chapter_title">Charpter.</span>
+								<span class="class_tab_content_chapter_title" style="margin-left: 5px;">
+									{{ item.ChapterTitle }}
+								</span>
+							</summary>
+							<div v-for="(item2, item2Index) in item.lessons" class="class_tab_content_lesson_title">
+								<!-- <span>Lesson</span> -->
+								<p>lessons.{{item2Index + 1}} : {{ item2.LessonTitle }}</p>
+							</div>
+							<div class="class_tab_content_lesson_content">
+								<p>{{ classCuriData.LessonContent }}</p>
+							</div>
+						</details>
 					</div>
+					
 				</div>	
 			</div>
 			
