a822dd4dc4a6dc09a277c2005dadecf0ca1140cc
choibocknami12 <h_hcho1215@naver.com>
2024-01-02 11:22:02 +0900

    0102현

--- Class4You/resources/components/ClassBoardDetailComponent.vue
+++ Class4You/resources/components/ClassBoardDetailComponent.vue
@@ -163,15 +163,16 @@
 					<div class="class_tab_content_title">
 						<p>커리큘럼</p>
 					</div>
-					<div id="class_tab1" class="class_tab_content">
-						<div v-for="item in allLessonsData" class="class_tab_content_curriculum" style="display: inline-block;">
-							<div  class="class_tab_content_curriculm_title" style="margin: 20px 0px;">
+					<div v-for="item in allLessonsData" id="class_tab1" class="class_tab_curri_div">
+						<div class="class_tab_content_curriculum">
+							<div class="class_tab_content_curriculm_title" style="margin: 10px 0px;">
 								<span class="class_tab_content_chapter_title">Charpter.</span>
 								<span class="class_tab_content_chapter_title" style="margin-left: 5px;">
 									{{ item.ChapterTitle }}
 								</span>
 							</div>
-							<div  v-for="item2 in item.lessons" class="class_tab_content_lesson_title">
+							<div v-for="item2 in item.lessons" class="class_tab_content_lesson_title">
+								<span>Lesson</span>
 								<p>
 									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
   										<path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z"/>
