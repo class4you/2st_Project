2e1286b35eb0729d2016da1c20dad801bac17a97
iammyeongho <jmh4912@naver.com>
2024-01-08 16:12:15 +0900

    0108 언어태그 작업

--- Class4You/resources/components/BoardComponent.vue
+++ Class4You/resources/components/BoardComponent.vue
@@ -33,15 +33,15 @@
 											</a>
 										</li>
 										<li class="community_aside_menu">
-										<a @click="fetchData(1, searchQuery = null, solve = null, sortData = 1, boardCategory = 2)" :class="{ 'community_aside_menu_on': boardCategory == 2}">질문게시판</a>
-											<ul class="community_dropdown">
+										<a @click="fetchData(1, searchQuery, solve = null, sortData = 1, boardCategory = 2)" :class="{ 'community_aside_menu_on': boardCategory == 2}">질문게시판</a>
+											<!-- <ul class="community_dropdown">
 												<li><a @click="fetchData(1, searchQuery, solve = null, sortData, boardCategory = 2, boardLanguage = 'HTML')">HTML</a></li>
 												<li><a @click="fetchData(1, searchQuery, solve = null, sortData, boardCategory = 2, boardLanguage = 'CSS')">CSS</a></li>
 												<li><a @click="fetchData(1, searchQuery, solve = null, sortData, boardCategory = 2, boardLanguage = 'JavaScript')">Java Script</a></li>
 												<li><a @click="fetchData(1, searchQuery, solve = null, sortData, boardCategory = 2, boardLanguage = 'PHP')">PHP</a></li>
 												<li><a @click="fetchData(1, searchQuery, solve = null, sortData, boardCategory = 2, boardLanguage = 'JAVA')">JAVA</a></li>
 												<li><a @click="fetchData(1, searchQuery, solve = null, sortData, boardCategory = 2, boardLanguage = 'DataBase')">DataBase</a></li>
-											</ul> 
+											</ul>  -->
 										</li>
 									</ul>
 									</li>
@@ -70,106 +70,17 @@
 								</ul>
 								<div class="e_search">
 
-									<fieldset class="checkbox-group">
-										<div class="checkbox">
-											<label class="checkbox-wrapper">
-												<input type="checkbox" class="checkbox-input" />
-												<span class="checkbox-tile">
-													<span class="checkbox-icon">
-														
-														<svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="currentColor" viewBox="0 0 256 256">
-															<rect width="256" height="256" fill="none"></rect>
-															<circle cx="96" cy="144.00002" r="10"></circle>
-															<circle cx="160" cy="144.00002" r="10"></circle>
-															<path d="M74.4017,80A175.32467,175.32467,0,0,1,128,72a175.32507,175.32507,0,0,1,53.59754,7.99971" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></path>
-															<path d="M181.59717,176.00041A175.32523,175.32523,0,0,1,128,184a175.32505,175.32505,0,0,1-53.59753-7.99971" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></path>
-															<path d="M155.04392,182.08789l12.02517,24.05047a7.96793,7.96793,0,0,0,8.99115,4.20919c24.53876-5.99927,45.69294-16.45908,61.10024-29.85086a8.05225,8.05225,0,0,0,2.47192-8.38971L205.65855,58.86074a8.02121,8.02121,0,0,0-4.62655-5.10908,175.85294,175.85294,0,0,0-29.66452-9.18289,8.01781,8.01781,0,0,0-9.31925,5.28642l-7.97318,23.91964" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></path>
-															<path d="M100.95624,182.08757l-12.02532,24.0508a7.96794,7.96794,0,0,1-8.99115,4.20918c-24.53866-5.99924-45.69277-16.459-61.10006-29.85069a8.05224,8.05224,0,0,1-2.47193-8.38972L50.34158,58.8607a8.0212,8.0212,0,0,1,4.62655-5.1091,175.85349,175.85349,0,0,1,29.66439-9.18283,8.0178,8.0178,0,0,1,9.31924,5.28642l7.97318,23.91964" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></path>
-														</svg>
-													</span>
-													<span class="checkbox-label">HTML</span>
-												</span>
-											</label>
-										</div>
-										<div class="checkbox">
-											<label class="checkbox-wrapper">
-												<input type="checkbox" class="checkbox-input" checked />
-												<span class="checkbox-tile">
-													<span class="checkbox-icon">
-														<svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="currentColor" viewBox="0 0 256 256">
-															<rect width="256" height="256" fill="none"></rect>
-															<polygon points="56 100 56 168 128 236 128 168 200 168 56 32 200 32 200 100 56 100" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></polygon>
-														</svg>
-													</span>
-													<span class="checkbox-label">CSS</span>
-												</span>
-											</label>
-										</div>
-										<div class="checkbox">
-											<label class="checkbox-wrapper">
-												<input type="checkbox" class="checkbox-input" />
-												<span class="checkbox-tile">
-													<span class="checkbox-icon">
-														<svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="currentColor" viewBox="0 0 256 256">
-															<rect width="256" height="256" fill="none"></rect>
-															<polygon points="72 40 184 40 240 104 128 224 16 104 72 40" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></polygon>
-															<polygon points="177.091 104 128 224 78.909 104 128 40 177.091 104" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></polygon>
-															<line x1="16" y1="104" x2="240" y2="104" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></line>
-														</svg>
-													</span>
-													<span class="checkbox-label">JavaScript</span>
-												</span>
-											</label>
-										</div>
-										<div class="checkbox">
-											<label class="checkbox-wrapper">
-												<input type="checkbox" class="checkbox-input" />
-												<span class="checkbox-tile">
-													<span class="checkbox-icon">
-													
-														<svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="currentColor" viewBox="0 0 256 256">
-															<rect width="256" height="256" fill="none"></rect>
-															<circle cx="128" cy="128" r="40" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></circle>
-															<rect x="36" y="36" width="184" height="184" rx="48" stroke-width="12" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" fill="none"></rect>
-															<circle cx="180" cy="75.99998" r="10"></circle>
-														</svg>
-													</span>
-													<span class="checkbox-label">PHP</span>
-												</span>
-											</label>
-										</div>
-										<div class="checkbox">
-											<label class="checkbox-wrapper">
-												<input type="checkbox" class="checkbox-input" />
-												<span class="checkbox-tile">
-													<span class="checkbox-icon">
-													</span>
-													<span class="checkbox-label">JAVA</span>
-												</span>
-											</label>
-										</div>
-										<div class="checkbox">
-											<label class="checkbox-wrapper">
-												<input type="checkbox" class="checkbox-input" />
-												<span class="checkbox-tile">
-													<span class="checkbox-icon">
-														<svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="currentColor" viewBox="0 0 256 256">
-															<rect width="256" height="256" fill="none"></rect>
-															<circle cx="128" cy="128" r="96" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></circle>
-															<g>
-																<path d="M179.1333,108.32931a112.19069,112.19069,0,0,0-102.3584.04859" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></path>
-																<path d="M164.29541,136.71457a79.94058,79.94058,0,0,0-72.68359.04736" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></path>
-																<path d="M149.47217,165.07248a47.97816,47.97816,0,0,0-43.03662.04736" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></path>
-															</g>
-														</svg>
-													</span>
-													<span class="checkbox-label">DataBase</span>
-												</span>
-											</label>
+									<fieldset v-if="boardCategory == 2" class="checkbox-group">
+										<div class="checkbox" v-for="(item, index) in checkboxes" :key="index">
+										<label class="checkbox-wrapper">
+											<input type="checkbox" class="checkbox-input" v-model="checkboxes[index].checked" :value="item.label" />
+											<span class="checkbox-tile">
+											<span class="checkbox-label">{{ item.label }}</span>
+											</span>
+										</label>
 										</div>
 									</fieldset>
 
-									
 									<div class="search_item">
 									<div class="ac_input_with_item">
 										<input type="text" v-model="searchQuery">
@@ -229,10 +140,14 @@
 											<p class="question_content_body">
 												{{ item.BoardComment }}
 											</p>
-											<div class="question_tags">
-											<!-- <button class="question_tags_button">
-												언어 태그란
-											</button> -->
+											<div v-if="boardCategory == 2" class="question_tags">
+												<div class="question_tags_button">언어 태그</div>
+												<div class="question_tags_button_on" v-if="item.HTMLFlg == 1">HTML</div>
+        										<div class="question_tags_button_on" v-if="item.CSSFlg == 1">CSS</div>
+        										<div class="question_tags_button_on" v-if="item.JavaScriptFlg == 1">JavaScript</div>
+        										<div class="question_tags_button_on" v-if="item.PHPFlg == 1">PHP</div>
+        										<div class="question_tags_button_on" v-if="item.JAVAFlg == 1">JAVA</div>
+        										<div class="question_tags_button_on" v-if="item.DataBaseFlg == 1">DataBase</div>
 											</div>
 											<div class="question_info_footer">
 											<div class="question_info_detail">
@@ -308,7 +223,6 @@
 				</div>
 		</div>
 	</div>
-
 </div>
 </template>
 <script>
@@ -331,7 +245,17 @@ export default {
 			current_page: 1,
 			pageChk: {},
 			boardCategory: 1,
-			boardLanguage: '',
+
+			checkboxes: [
+				{ label: 'HTML', checked: false },
+				{ label: 'CSS', checked: false },
+				{ label: 'JavaScript', checked: false },
+				{ label: 'PHP', checked: false },
+				{ label: 'JAVA', checked: false },
+				{ label: 'DataBase', checked: false },
+			],
+
+
 		}
 	},
 	
@@ -339,9 +263,15 @@ export default {
         this.fetchData();
     },
 	methods: {
-		// 해당 함수가 실행 될때 파라미터를 보냄, 엑시오스 처리할 때 파라미터를 포함해서 라우터로 보냄
-		fetchData(page = 1, searchQuery = '', solve = null, sortData = 1, boardCategory = 1, boardLanguage = null) {
-        axios.get(`/board/data?page=${page}&search=${searchQuery}&solve=${solve}&sort=${sortData}&boardCategory=${boardCategory}&boardLanguage=${boardLanguage}`)
+			// 해당 함수가 실행 될때 파라미터를 보냄, 엑시오스 처리할 때 파라미터를 포함해서 라우터로 보냄
+		fetchData(page = 1) {
+
+		const selectedLanguages = this.checkboxes
+		.filter(item => item.checked)
+		.map(item => item.label)
+		.join(',')
+		
+        axios.get(`/board/data?page=${page}&search=${this.searchQuery}&solve=${this.solve}&sort=${this.sortData}&boardCategory=${this.boardCategory}&boardLanguage=${selectedLanguages}`)
             .then(response => {
 			
                 this.newBoardItems = response.data.boardData.data;
@@ -349,7 +279,7 @@ export default {
                 this.pagination = response.data.boardData.links;
                 this.page = response.data.boardData.current_page;
                 this.pageChk = response.data.boardData.current_page;
-				// console.log(response.data);
+				console.log(response.data);
 				// console.log(response.data.boardData.data);
 				// console.log(response.data.boardData.links);
 				// console.log(response.data.boardData.current_page);
@@ -413,8 +343,17 @@ export default {
                 // 에러 처리
                 console.error(error);
             });
-		}
-	}
+		},
+	},
+	watch: {
+		checkboxes: {
+			deep: true,
+			handler() {
+				this.fetchData();
+				// this.sendRequest();
+			},
+		},
+	},
 }
 
 </script>
