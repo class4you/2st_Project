fa08825115bfbc84764d86a748e84bbbdba10725
choibocknami12 <h_hcho1215@naver.com>
2024-01-17 10:55:21 +0900

    0117css

--- Class4You/resources/components/ClassBoardDetailComponent.vue
+++ Class4You/resources/components/ClassBoardDetailComponent.vue
@@ -861,7 +861,7 @@
 						<div class="class_detail_commu_list_text_div2" v-else>
 							<div class="class_detail_commu_list_text_board_title" style="display: flex;">
 								<span class="class_detail_commu_list_text_board_tag">
-									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16">
+									<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16">
   										<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
   										<path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
 									</svg>
@@ -876,7 +876,12 @@
 						<div v-if="data.UserID == $store.state.UserID">
 							<div class="class_detail_commu_list_user" style="font-size: 13px;">
 								<div class="class_detail_commu_user_id">
-									<div class="class_detail_rating_user_id_info">
+									<div class="class_detail_rating_user_id_info" style="display: flex;">
+										<span>
+											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-circle-fill" viewBox="0 0 16 16">
+												<circle cx="8" cy="8" r="8"/>
+											</svg>
+										</span>
 										<p class="class_detail_rating_user_email">{{ hideEmail(data.UserEmail) }}</p>
 									</div>
 									<div class="class_detail_rating_user_date">
