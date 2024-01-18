2e21e00b98e9df392f3203c6b9e7502541e79da3
iammyeongho <jmh4912@naver.com>
2024-01-18 17:53:55 +0900

    0118 모달 확인

--- Class4You/resources/components/AdminBoardQuestionDataComponent.vue
+++ Class4You/resources/components/AdminBoardQuestionDataComponent.vue
@@ -426,25 +426,6 @@
             <i class="fas fa-angle-up"></i>
             </a>
 
-            <!-- Logout Modal-->
-            <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                        <a class="btn btn-primary" href="login.html">Logout</a>
-                    </div>
-                </div>
-            </div>
-            </div>
     </div>
 </template>
 <script>
@@ -603,6 +584,10 @@ export default {
     width: 100vw;
     height: 100vh;
     z-index: 100;
+}
+
+.admin_modal_show {
+    display: block;
 }
 
 .admin_modal_content_label_span{
