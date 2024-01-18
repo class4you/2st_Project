6c21d3c95e0ee2abdf37ad37ed36f4e64d8dc39e
iammyeongho <jmh4912@naver.com>
2024-01-18 12:41:48 +0900

    0118 어드민 오류 해결

--- Class4You/resources/components/AdminRegistrationComponent.vue
+++ Class4You/resources/components/AdminRegistrationComponent.vue
@@ -344,9 +344,9 @@
                                         </div>
                                         <div class="form-group row">
                                             <div style="display: flex; gap: 20px;" class="col-sm-6 mb-3 mb-sm-0">
-                                                <input style="text-align: center; font-size: 20px;" type="text" class="form-control form-control-user" id="exampleFirstName"placeholder="First Name">
-                                                <input style="text-align: center; font-size: 20px;" type="text" class="form-control form-control-user" id="exampleFirstName"placeholder="First Name">
-                                                <input style="text-align: center; font-size: 20px;" type="text" class="form-control form-control-user" id="exampleFirstName"placeholder="First Name">
+                                                <input style="text-align: center; font-size: 20px;" type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
+                                                <input style="text-align: center; font-size: 20px;" type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
+                                                <input style="text-align: center; font-size: 20px;" type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
                                             </div>
                                         </div>
                                         <div class="form-group row">
