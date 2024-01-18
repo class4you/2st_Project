ad5fb9bcfc229113ab2f240bcf6a9065c474d8c0
choibocknami12 <h_hcho1215@naver.com>
2024-01-18 12:42:18 +0900

    0118현

--- Class4You/resources/components/AdminRegistrationComponent.vue
+++ Class4You/resources/components/AdminRegistrationComponent.vue
@@ -344,9 +344,9 @@
                                         </div>
                                         <div class="form-group row">
                                             <div style="display: flex; gap: 20px;" class="col-sm-6 mb-3 mb-sm-0">
-                                                <input style="text-align: center; font-size: 20px;" type="text" class="form-control form-control-user" id="exampleFirstName"placeholder="First Name">
-                                                <input style="text-align: center; font-size: 20px;" type="text" class="form-control form-control-user" id="exampleFirstName"placeholder="First Name">
-                                                <input style="text-align: center; font-size: 20px;" type="text" class="form-control form-control-user" id="exampleFirstName"placeholder="First Name">
+                                                <input style="text-align: center; font-size: 20px;" type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
+                                                <input style="text-align: center; font-size: 20px;" type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
+                                                <input style="text-align: center; font-size: 20px;" type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
                                             </div>
                                         </div>
                                         <div class="form-group row">
@@ -440,7 +440,7 @@ export default {
             axios.get('/instructormain')
             .then(response => {
                 console.log(response.data);
-               
+            
             })
             .catch(error => {
                 // console.error('Error fetching data:', error);
