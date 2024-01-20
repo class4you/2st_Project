68dd390a7203529e0e01c8c3c489d1fe43c9f712
choibocknami12 <h_hcho1215@naver.com>
2024-01-20 19:03:55 +0900

    0120css

--- Class4You/resources/components/AdminBoardQuestionDataComponent.vue
+++ Class4You/resources/components/AdminBoardQuestionDataComponent.vue
@@ -531,6 +531,7 @@ export default {
             axios.post(url, frm, header)
                 .then(res => {
                     console.log("questionAnswerData",res);
+                    // this.showModal = false;
                 })
                 .catch(err => {
 
