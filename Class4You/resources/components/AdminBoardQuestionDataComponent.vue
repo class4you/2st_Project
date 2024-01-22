f52edeb81e15a23191baceef69822fb75f4b0bcf
iammyeongho <jmh4912@naver.com>
2024-01-22 10:02:51 +0900

    0122 디자인 수정

--- Class4You/resources/components/AdminBoardQuestionDataComponent.vue
+++ Class4You/resources/components/AdminBoardQuestionDataComponent.vue
@@ -382,21 +382,27 @@
                                             </div> -->
                                             
                                             <div class="admin_modal_content_text">
-
-                                                <div class="admin_modal_content_label_span">
-                                                    <div class="admin_modal_content_label">
-                                                        <label for="">답변하기</label>
-                                                    </div>
-                                                    <div>
-                                                        <span @click="showModal = false" class="admin_answer_modal_close">❌</span>
-                                                    </div>
+                                                <div class="admin_class_user_data_modal-container-header">
+                                                    <h5 class="admin_class_user_data_modal-container-title">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true">
+                                                        <path fill="none" d="M0 0h24v24H0z" />
+                                                        <path fill="currentColor" d="M14 9V4H5v16h6.056c.328.417.724.785 1.18 1.085l1.39.915H3.993A.993.993 0 0 1 3 21.008V2.992C3 2.455 3.449 2 4.002 2h10.995L21 8v1h-7zm-2 2h9v5.949c0 .99-.501 1.916-1.336 2.465L16.5 21.498l-3.164-2.084A2.953 2.953 0 0 1 12 16.95V11zm2 5.949c0 .316.162.614.436.795l2.064 1.36 2.064-1.36a.954.954 0 0 0 .436-.795V13h-5v3.949z" />
+                                                    </svg>
+                                                    답변하기
+                                                    </h5>
+                                                    <button @click="showModal = false"   class="admin_class_user_data_icon-button">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                                        <path fill="none" d="M0 0h24v24H0z" />
+                                                        <path fill="currentColor" d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" />
+                                                    </svg>
+                                                    </button>
                                                 </div>
+
                                             
                                                 <textarea v-model="questionAnswerData.CommentContent"></textarea>
                                                     <!-- <textarea></textarea> -->
-                                                <div class="admin_modal_content_text_btn" style="text-align: end;">
-                                                    <button type="button" @click="submitAnswer()">전송</button>
-                                                    <!-- <button>전송</button> -->
+                                                <div class="admin_class_user_data_modal-container-footer">
+                                                    <button @click="submitAnswer()"  class="admin_class_user_data_button admin_class_user_data_is-primary">전송</button>
                                                 </div>
                                                 
                                             </div>
@@ -628,7 +634,7 @@ export default {
     height: 100vh;
     z-index: 100;
     opacity: 0.5; */
-    background-color: rgba(255,255,255,0.5);
+    background-color: rgba(0, 0, 0, 0.25);
     /* opacity: 0.5; */
 }
 
@@ -677,9 +683,10 @@ export default {
   border-radius: 5px;
   height: 30vh;
   padding: 10px;
-  background-color: #1c3264d6;
+  background-color: #ffffff;
   display: flex;
     flex-direction: column;
+    border-radius: 20px;
 }
 
 .admin_modal_content_text_btn>button {
